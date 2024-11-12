20805dd292268eaca156923039826af70b565d01
KhalidAlhes <164419838+KhalidAlhes@users.noreply.github.com>
2024-11-11 23:58:02 -0500

    buttons

--- nonimm-domain/src/main/java/edu/gmu/cs321/basicscreen.java
+++ nonimm-domain/src/main/java/edu/gmu/cs321/basicscreen.java
@@ -13,29 +13,31 @@ public class basicscreen extends JFrame {
         setTitle("EntryForm Screen");
         setSize(300, 200);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new GridLayout(3, 2));
+        setLayout(new GridLayout(4, 2));
 
-       
+        // labels for data
         formIdLabel = new JLabel("Form ID: " + entryForm.getformid());
         dateLabel = new JLabel("Date: " + entryForm.getdate());
         stepLabel = new JLabel(entryForm.getStep());
 
-        // labels for id date and step
+        // frame labels
         add(new JLabel("Form ID:"));
         add(formIdLabel);
         add(new JLabel("Date:"));
         add(dateLabel);
         add(new JLabel("Step:"));
         add(stepLabel);
-
+        //buttons
+        JButton approveButton = new JButton("Approve");
+        JButton denyButton = new JButton("Deny");
+        add(approveButton);
+        add(denyButton);
         setVisible(true);
     }
 
     public static void main(String[] args) {
-        
+        // test
         EntryForm entryForm = new EntryForm(123, 20231111, "Entry");
-
-
         new basicscreen(entryForm);
     }
 }
